6ecce5e211a3fa2dfa5ad9d3d1ddec902ddff28f
Michela Ledwidge <michela@thequality.com>
2013-07-27 13:25:37 +0000

    safety - working

--- server.py
+++ server.py
@@ -2,15 +2,32 @@ import tornado.httpserver
 import tornado.websocket
 import tornado.ioloop
 import tornado.web
+import re
 
 class WSHandler(tornado.websocket.WebSocketHandler):
     def open(self):
         print 'new connection'
-        self.write_message("Hello World")
+        self.write_message("%s: hello " % ('poll.modprods.com'))
       
     def on_message(self, message):
-        print 'message received %s' % message
- 
+#        print 'message received %s' % message
+	vote = re.match(r"/vote/(.*)",message)	
+	msg = re.match(r"/message/(.*)",message)
+	try:
+		if vote.group(1):
+			print("Vote: " + vote.group(1))
+			self.write_message("ok")
+	except Exception as e:
+		#print(e)
+		pass
+        try:
+                if msg.group(1):
+                        print("Message: " + msg.group(1))
+			self.write_message("ok")
+        except Exception as e:
+                #print(e)
+		pass
+
     def on_close(self):
       print 'connection closed'
 
